775d8fbea8f7c28d5b866fdde85ab47a97ac8b40
Moaaz-i <172604087+Moaaz-i@users.noreply.github.com>
2026-07-01 03:22:52 +0000

    style: automated code formatting by bot

--- src/Animation.h
+++ src/Animation.h
@@ -3,12 +3,10 @@
 
 // Feature 7: CSS Animation (Keyframes)
 struct Animation {
-    String name;
-    String keyframes;
+  String name;
+  String keyframes;
 
-    Animation(const String &n, const String &kf) : name(n), keyframes(kf) {}
+  Animation(const String &n, const String &kf) : name(n), keyframes(kf) {}
 
-    String toCSS() const {
-        return "@keyframes " + name + "{" + keyframes + "}";
-    }
+  String toCSS() const { return "@keyframes " + name + "{" + keyframes + "}"; }
 };
